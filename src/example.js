6f5f5c0607076c324d6be38efb0d11d1f5f44178
iAmNatch <shaner@snappe.io>
2021-06-19 22:47:10 -0400

    seperate example into own file

---
+++ src/example.js
@@ -0,0 +1,70 @@
+export const example = {
+  nodes: [
+    {
+      fk: "justabox",
+      type: "box",
+      title: "cool",
+      content: `hello world i am your ru`,
+    },
+    {
+      fk: "first",
+      type: "uml",
+      title: "Bae",
+      content: [
+        [
+          `String [string] dsfkljdfsklj`,
+          "Number! [number]",
+          "long long long [long] dddddddd",
+        ],
+        ["Bling [bling]", "Uber", "long long long long long long [long]"],
+      ],
+    },
+    {
+      fk: "second",
+      type: "uml",
+      title: "Hello lk",
+      content: [
+        [
+          "String [string]",
+          "Number! [number]",
+          "long long long [long] dddddddd",
+        ],
+        ["Bling [bling]", "Uber", "long long long long long long [long]"],
+      ],
+    },
+    {
+      fk: "third",
+      type: "uml",
+      title: "Hello lk",
+      content: [
+        [
+          "String [string]",
+          "Number! [number]",
+          "long long long [long] dddddddd",
+        ],
+        ["Bling [bling]", "Uber", "long long long long long long [long]"],
+      ],
+    },
+    {
+      fk: "something_elssee",
+      type: "uml",
+      title: "Hello dear sir",
+      content: [
+        [
+          "String [string]",
+          "Number! [number]",
+          "long long long [long] dddddddd",
+          "Number! [number]",
+          "long long long [long] dddddddd",
+          "Number! [number]",
+          "long long long [long] dddddddd",
+        ],
+        ["Bling [bling]", "Uber", "long long long long long long [long]"],
+      ],
+    },
+  ],
+  connections: [
+    { id: "e1", label: "hell world", sources: ["first"], targets: ["second"] },
+    { id: "e2", label: "hell world", sources: ["first"], targets: ["third"] },
+  ],
+};
